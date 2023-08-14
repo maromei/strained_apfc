6f85901c2395ea84cab6d1cf4b3a28535d718212
maromei <max@pop-os.localdomain>
2023-08-14 10:30:18 +0200

    use commit counting as revision

--- scripts/write_version_to_file.py
+++ scripts/write_version_to_file.py
@@ -9,15 +9,23 @@ VERSION_FILE = PROJECT_ROOT / "src/__about__.py"
 repo = git.Repo(PROJECT_ROOT)
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 
-latest_tag = str(tags[-1])
-commit_count_since_tag = repo.git.rev_list("--count", f"{latest_tag}..HEAD")
-commit_count_since_tag = int(commit_count_since_tag)
+if len(tags) > 0:
+
+    latest_tag = str(tags[-1])
+    commit_count_since_tag = repo.git.rev_list("--count", f"{latest_tag}..HEAD")
+    commit_count_since_tag = int(commit_count_since_tag)
+
+else:
+
+    latest_tag = "0.0"
+    commit_count_since_tag = repo.git.rev_list("--count", "HEAD")
+    commit_count_since_tag = int(commit_count_since_tag)
 
 if latest_tag[0] == "v":
     latest_tag = latest_tag[1:]
 
 if commit_count_since_tag != 0:
-    latest_tag += f"-dev{commit_count_since_tag+1}"
+    latest_tag += f".{commit_count_since_tag + 1}"
 
 with open(VERSION_FILE, "w") as f:
     f.write(f'__version__ = "{latest_tag}"\n')
